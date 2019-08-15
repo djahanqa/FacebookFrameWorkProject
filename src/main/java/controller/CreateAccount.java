c3dc2e57d73340a48aee51a6c119d8f4110cd4a8
dilrubajahan <djahanqa@gmail.com>
2019-08-15 00:13:23 -0400

    Create account test on facebook

--- src/main/java/controller/CreateAccount.java
+++ src/main/java/controller/CreateAccount.java
@@ -1,4 +1,20 @@
 package controller;
 
-public class CreateAccount {
+import base.ScriptBase;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreateAccount extends ScriptBase {
+    @FindBy (xpath = "//*[@name='firstname']")
+    WebElement firstname;
+
+    public CreateAccount(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+public void creatingAccount(String fname){
+
+        firstname.sendKeys(fname);
+}
 }
---
+++ src/test/java/createaccount/CreateAccountTest.java
@@ -0,0 +1,34 @@
+package createaccount;
+
+import base.ScriptBase;
+import controller.CreateAccount;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+
+public class CreateAccountTest extends ScriptBase{
+
+
+        CreateAccount createAccount;
+
+        @BeforeTest
+        public void beforetest() {
+            init();
+
+        }
+
+        @Test
+        public void varifycreatingAccount() {
+            createAccount = new CreateAccount(driver);
+            createAccount.creatingAccount("mohshina");
+        }
+
+        @AfterTest
+        public void closebrowser() {
+//            driver.close();
+//            driver.quit();
+
+        }
+
+    }
